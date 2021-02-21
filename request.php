2406298f662628e297a59db269beb613f5d957be
hectlite <73349557+hectlite@users.noreply.github.com>
2021-02-21 17:22:19 +0300

    Add files via upload

--- request.php
+++ request.php
@@ -1,16 +1,14 @@
 <?php
-//get data from form
-$name = $_POST['name']
-$email = $_POST['email']
-$message = $_POST['message']
 $to = "hectlite@gmail.com";
-$subject ="Mail from hectlite";
-$txt ="Name = ". $name . "\r\n Email = " .$email . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@hectlite.com" . "\r\n .
-"CC: somebodyelse@example.com";
-if($email!=NULL){
-	mail($to,$subject,$txt,$headers);
+$subject = "Response from website";
+$message = "you just requested for a software !!! your patience succeeds our mission";
+$headers ="From: noreply@hectlite.tk";
+
+
+if (mail ($to, $subject, $message, $headers) ) {
+	echo "request succeeded";
+}  else {
+	echo "request unable to be processed";	
 }
-//redirect
-header("location:thankyou.html");
+
 ?>
